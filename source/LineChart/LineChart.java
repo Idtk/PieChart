5d6498064dec5c46f89b1e01dff51317dbbe7ab0
Idtk <623680332@qq.com>
2016-04-23 23:42:38 +0800

    LineChart

--- source/LineChart/LineChart.java
+++ source/LineChart/LineChart.java
@@ -2,10 +2,14 @@ package com.example.administrator.customview.LineChart;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by DoBest on 2016/4/21.
@@ -17,17 +21,25 @@ public class LineChart extends View {
     private int mHeight;
     private ArrayList<LineData> mLineDatas = new ArrayList<>();
     private float[] coordinate = new float[6];
+    private Paint mPaint = new Paint();
 
     public LineChart(Context context) {
         super(context);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setAntiAlias(true);
+
     }
 
     public LineChart(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setAntiAlias(true);
     }
 
     public LineChart(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setAntiAlias(true);
     }
 
     @Override
@@ -39,18 +51,41 @@ public class LineChart extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
+        mPaint.setColor(Color.BLACK);
+        mPaint.setTextSize(5f);
+        float x = mWidth*3/5;
+        float y = mHeight*3/5;
+        canvas.translate(mWidth/5,mHeight*4/5);
+        canvas.scale(1,-1);
+        canvas.drawLine(0,0,x,0,mPaint);
+        canvas.drawLine(0,0,0,y,mPaint);
+//        canvas.drawLine(x/3,0,x/3,y,mPaint);
+        int timesX = (int) Math.floor(x/(coordinate[1]-coordinate[0]));
+        Log.i("TAG", Arrays.toString(coordinate));
+        for (int i=0;(coordinate[2]*i+coordinate[0])<=coordinate[1];i++){
+            canvas.drawLine((coordinate[2]*i+coordinate[0])*timesX,0,
+                    (coordinate[2]*i+coordinate[0])*timesX,y/50,mPaint);
+            Log.i("TAGX",(coordinate[2]*i+coordinate[0])*timesX+"");
+        }
+        int timesY = (int) Math.floor(y/(coordinate[4]-coordinate[3]));
+        for (int i=0;(coordinate[5]*i+coordinate[3])<=coordinate[4];i++){
+            canvas.drawLine(0,(coordinate[5]*i+coordinate[3])*timesY,
+                    x/50,(coordinate[5]*i+coordinate[3])*timesY,mPaint);
+            Log.i("TAGY",(coordinate[5]*i+coordinate[3])*timesY+"");
+        }
         super.onDraw(canvas);
-
     }
 
     public void setLineDatas(ArrayList<LineData> lineDatas) {
         mLineDatas = lineDatas;
+        initCoordinate(mLineDatas);
+        invalidate();
     }
 
     /**
      * 计算坐标系
      */
-    private void initCoordinate(){
+    private void initCoordinate(ArrayList<LineData> mLineDatas){
         for (int i=0; i<mLineDatas.size();i++){
             LineData mLineData = mLineDatas.get(i);
             int length = mLineData.getValue().length;
@@ -63,7 +98,7 @@ public class LineChart extends View {
             initMaxMin(mLineData,maxX,minX,scalingX,true,i);
             initAxis(1,mLineData,maxY,minY,scalingY,i);
         }
-//        Log.i("TAG", Arrays.toString(coordinate));
+//        Log.i("TAG2", Arrays.toString(coordinate));
     }
 
     /**
